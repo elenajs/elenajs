749e7161d8a5a78b527ff541fa866cb0bdaa0590
Andrea Chiumenti <kiuma72@gmail.com>
2014-11-07 14:45:36 +0100

    fixing httpContext bug (wrong context)

--- demo/Server.js
+++
@@ -1,9 +0,0 @@
-define([
-    "dojo/_base/declare",
-    "elenajs/Server",
-    "./_dispatchers"
-], function(declare, _Server, dispatchers) {
-    return declare('demo/Server', _Server, {
-        dispatchers: dispatchers
-    });
-});
--- demo/_dispatchers.js
+++
@@ -1,9 +0,0 @@
-define([
-    "./dispatchers/StaticResources",
-    "./dispatchers/PageIndex",
-    "./dispatchers/PageForm"
-], function () {
-    var result = Array.prototype.slice.call(arguments).map(function (Clazz) { return new Clazz();});
-    return result;
-});
-
